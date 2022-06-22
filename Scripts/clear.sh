1927ab1551e8349d7b34dd04c94fd78ddd2ba1e4
gmarseglia <g.marseglia.it@gmail.com>
2022-06-22 17:51:52 +0200

    Jupyter test.

--- Scripts/clear.sh
+++ Scripts/clear.sh
@@ -1,15 +1,10 @@
 # !/bin/bash
-for d in $(find . -type d -name "__pycache__") ; do
+for d in $(find . -type d -name "__pycache__" -or -name ".ipynb_checkpoints") ; do
 	trash "$d"
 	echo "$d deleted."
 done
 
-for f in $(find . -type f -name "*.sage.py") ; do
-	trash "$f"
-	echo "$f deleted."
-done
-
-for f in $(find . -type f -name "*.old") ; do
+for f in $(find . -type f -name "*.sage.py" -or -name "*.old") ; do
 	trash "$f"
 	echo "$f deleted."
 done
